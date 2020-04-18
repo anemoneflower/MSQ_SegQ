159118a6647a6de6f040b8649c6843ade5111f27
anemoneflower <sallylucky@naver.com>
2020-04-18 12:49:44 +0900

    add

---
+++ src/msqueue.rs
@@ -0,0 +1,364 @@
+//maqueue => skeleton from cs492-concur-master/lockfree/queue
+
+use core::mem::{self, ManuallyDrop};
+use core::ptr;
+use core::sync::atomic::Ordering;
+
+use crossbeam_epoch::{unprotected, Atomic, Guard, Owned, Shared};
+use crossbeam_utils::CachePadded;
+
+#[derive(Debug)]
+pub struct MSQueue<T> {
+    head: CachePadded<Atomic<Node<T>>>,
+    tail: CachePadded<Atomic<Node<T>>>,
+}
+
+#[derive(Debug)]
+struct Node<T> {
+
+    data: ManuallyDrop<T>,
+    next: Atomic<Node<T>>,
+}
+
+// Any particular `T` should never be accessed concurrently, so no need for `Sync`.
+unsafe impl<T: Send> Sync for MSQueue<T> {}
+unsafe impl<T: Send> Send for MSQueue<T> {}
+
+impl<T> MSQueue<T> {
+    /// Create a new, empty queue.
+    pub fn new() -> MSQueue<T> {
+        let q = MSQueue{
+            head: CachePadded::new(Atomic::null()),
+            tail: CachePadded::new(Atomic::null())
+        };
+        let sentinel = Owned::new(Node{
+            data: unsafe{mem::uninitialized()},
+            next: Atomic::null(),
+        });
+        unsafe{let guard = &unprotected();//epoch::pin();
+        let sentinel = sentinel.into_shared(&guard);
+        q.head.store(sentinel, Ordering::Relaxed);
+        q.tail.store(sentinel, Ordering::Relaxed);}
+        q
+    }
+
+    /// Adds `t` to the back of the queue, possibly waking up threads blocked on `pop`.
+    pub fn push(&self, t: T, guard: &Guard) {
+        let node = Owned::new(Node{
+            data: ManuallyDrop::new(t),
+            next: Atomic::null(),
+        });
+        let node = node.into_shared(guard);
+        loop{
+            //load acquire tail
+            let tail = self.tail.load(Ordering::Acquire, guard);
+
+            //check if this is real tail
+            let tail_ref = unsafe{tail.deref()};
+            let next = tail_ref.next.load(Ordering::Acquire, guard);
+            if !next.is_null(){
+                //move tail pointer forward
+                let _ = self.tail.compare_and_set(tail, next, Ordering::Release, guard);
+                continue;
+            }
+
+            if tail_ref.next.compare_and_set(Shared::null(), node, Ordering::Release, guard).is_ok(){
+                let _ = self.tail.compare_and_set(tail, node, Ordering::Release, guard);
+                break;
+            }
+
+            //unimplemented!()
+        }
+    }
+
+    /// Attempts to dequeue from the front.
+    ///
+    /// Returns `None` if the queue is observed to be empty.
+    pub fn try_pop(&self, guard: &Guard) -> Option<T> {
+        loop{
+            let head = self.head.load(Ordering::Acquire, guard);
+            let next = unsafe{head.deref()}.next.load(Ordering::Acquire, guard);
+            let nextref = unsafe{next.as_ref()};
+            if !nextref.is_some(){
+                return None;
+            }
+
+            //Move tail
+            let tail = self.tail.load(Ordering::Acquire, guard);
+            if tail == head {
+                let _ = self.tail.compare_and_set(tail, next, Ordering::Release, guard);
+            }
+
+            if self.head.compare_and_set(head, next, Ordering::Release, guard).is_ok(){
+                unsafe{guard.defer_destroy(head);
+                return Some(ManuallyDrop::into_inner(ptr::read(&nextref.unwrap().data)));}
+            }
+        }
+        //unimplemented!()
+
+    }
+}
+
+impl<T> Drop for MSQueue<T> {
+    fn drop(&mut self) {
+        unsafe {
+            let guard = unprotected();
+
+            while let Some(_) = self.try_pop(guard) {}
+
+            // Destroy the remaining sentinel node.
+            let sentinel = self.head.load(Ordering::Relaxed, guard);
+            drop(sentinel.into_owned());
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crossbeam_epoch::pin;
+    use crossbeam_utils::thread;
+
+    struct MSQueue<T> {
+        queue: super::MSQueue<T>,
+    }
+
+    impl<T> MSQueue<T> {
+        pub fn new() -> MSQueue<T> {
+            MSQueue {
+                queue: super::MSQueue::new(),
+            }
+        }
+
+        pub fn push(&self, t: T) {
+            let guard = &pin();
+            self.queue.push(t, guard);
+        }
+
+        pub fn is_empty(&self) -> bool {
+            let guard = &pin();
+            let head = self.queue.head.load(Ordering::Acquire, guard);
+            let h = unsafe { head.deref() };
+            h.next.load(Ordering::Acquire, guard).is_null()
+        }
+
+        pub fn try_pop(&self) -> Option<T> {
+            let guard = &pin();
+            self.queue.try_pop(guard)
+        }
+
+        pub fn pop(&self) -> T {
+            loop {
+                match self.try_pop() {
+                    None => continue,
+                    Some(t) => return t,
+                }
+            }
+        }
+    }
+
+    const CONC_COUNT: i64 = 1000000;
+
+    #[test]
+    fn push_try_pop_1() {
+        let q: MSQueue<i64> = MSQueue::new();
+        assert!(q.is_empty());
+        q.push(37);
+        assert!(!q.is_empty());
+        assert_eq!(q.try_pop(), Some(37));
+        assert!(q.is_empty());
+    }
+
+    #[test]
+    fn push_try_pop_2() {
+        let q: MSQueue<i64> = MSQueue::new();
+        assert!(q.is_empty());
+        q.push(37);
+        q.push(48);
+        assert_eq!(q.try_pop(), Some(37));
+        assert!(!q.is_empty());
+        assert_eq!(q.try_pop(), Some(48));
+        assert!(q.is_empty());
+    }
+
+    #[test]
+    fn push_try_pop_many_seq() {
+        let q: MSQueue<i64> = MSQueue::new();
+        assert!(q.is_empty());
+        for i in 0..200 {
+            q.push(i)
+        }
+        assert!(!q.is_empty());
+        for i in 0..200 {
+            assert_eq!(q.try_pop(), Some(i));
+        }
+        assert!(q.is_empty());
+    }
+
+    #[test]
+    fn push_pop_1() {
+        let q: MSQueue<i64> = MSQueue::new();
+        assert!(q.is_empty());
+        q.push(37);
+        assert!(!q.is_empty());
+        assert_eq!(q.pop(), 37);
+        assert!(q.is_empty());
+    }
+
+    #[test]
+    fn push_pop_2() {
+        let q: MSQueue<i64> = MSQueue::new();
+        q.push(37);
+        q.push(48);
+        assert_eq!(q.pop(), 37);
+        assert_eq!(q.pop(), 48);
+    }
+
+    #[test]
+    fn push_pop_many_seq() {
+        let q: MSQueue<i64> = MSQueue::new();
+        assert!(q.is_empty());
+        for i in 0..200 {
+            q.push(i)
+        }
+        assert!(!q.is_empty());
+        for i in 0..200 {
+            assert_eq!(q.pop(), i);
+        }
+        assert!(q.is_empty());
+    }
+
+    #[test]
+    fn push_try_pop_many_spsc() {
+        let q: MSQueue<i64> = MSQueue::new();
+        assert!(q.is_empty());
+
+        thread::scope(|scope| {
+            scope.spawn(|_| {
+                let mut next = 0;
+
+                while next < CONC_COUNT {
+                    if let Some(elem) = q.try_pop() {
+                        assert_eq!(elem, next);
+                        next += 1;
+                    }
+                }
+            });
+
+            for i in 0..CONC_COUNT {
+                q.push(i)
+            }
+        })
+        .unwrap();
+    }
+
+    #[test]
+    fn push_try_pop_many_spmc() {
+        fn recv(_t: i32, q: &MSQueue<i64>) {
+            let mut cur = -1;
+            for _i in 0..CONC_COUNT {
+                if let Some(elem) = q.try_pop() {
+                    assert!(elem > cur);
+                    cur = elem;
+
+                    if cur == CONC_COUNT - 1 {
+                        break;
+                    }
+                }
+            }
+        }
+
+        let q: MSQueue<i64> = MSQueue::new();
+        assert!(q.is_empty());
+        thread::scope(|scope| {
+            for i in 0..3 {
+                let q = &q;
+                scope.spawn(move |_| recv(i, q));
+            }
+
+            scope.spawn(|_| {
+                for i in 0..CONC_COUNT {
+                    q.push(i);
+                }
+            });
+        })
+        .unwrap();
+    }
+
+    #[test]
+    fn push_try_pop_many_mpmc() {
+        enum LR {
+            Left(i64),
+            Right(i64),
+        }
+
+        let q: MSQueue<LR> = MSQueue::new();
+        assert!(q.is_empty());
+
+        thread::scope(|scope| {
+            for _t in 0..2 {
+                scope.spawn(|_| {
+                    for i in CONC_COUNT - 1..CONC_COUNT {
+                        q.push(LR::Left(i))
+                    }
+                });
+                scope.spawn(|_| {
+                    for i in CONC_COUNT - 1..CONC_COUNT {
+                        q.push(LR::Right(i))
+                    }
+                });
+                scope.spawn(|_| {
+                    let mut vl = vec![];
+                    let mut vr = vec![];
+                    for _i in 0..CONC_COUNT {
+                        match q.try_pop() {
+                            Some(LR::Left(x)) => vl.push(x),
+                            Some(LR::Right(x)) => vr.push(x),
+                            _ => {}
+                        }
+                    }
+
+                    let mut vl2 = vl.clone();
+                    let mut vr2 = vr.clone();
+                    vl2.sort();
+                    vr2.sort();
+
+                    assert_eq!(vl, vl2);
+                    assert_eq!(vr, vr2);
+                });
+            }
+        })
+        .unwrap();
+    }
+
+    #[test]
+    fn push_pop_many_spsc() {
+        let q: MSQueue<i64> = MSQueue::new();
+
+        thread::scope(|scope| {
+            scope.spawn(|_| {
+                let mut next = 0;
+                while next < CONC_COUNT {
+                    assert_eq!(q.pop(), next);
+                    next += 1;
+                }
+            });
+
+            for i in 0..CONC_COUNT {
+                q.push(i)
+            }
+        })
+        .unwrap();
+        assert!(q.is_empty());
+    }
+
+    #[test]
+    fn is_empty_dont_pop() {
+        let q: MSQueue<i64> = MSQueue::new();
+        q.push(20);
+        q.push(20);
+        assert!(!q.is_empty());
+        assert!(!q.is_empty());
+        assert!(q.try_pop().is_some());
+    }
+}
